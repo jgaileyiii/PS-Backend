af1c511ee5abc2fb007ad4342e8d4e835186e7e7
Joe Gailey <jgaileyiii@gmail.com>
2020-04-16 12:45:55 -0600

    fixed conflicting port directions.

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors')
-const PORT = process.env.PORT || 4000;
+//const PORT = process.env.PORT || 4000;
 
 
 
@@ -18,11 +17,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors())
 
 app.use('/api/mailer', mailer)
 
-app.listen(PORT, host)
+//app.listen(PORT)
 
 
 // catch 404 and forward to error handler

6f4cef1b17e8bb46c2fdeea3a0083833770e1d0d
Joe Gailey <jgaileyiii@gmail.com>
2020-04-16 13:08:35 -0600

    added cors.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 //const PORT = process.env.PORT || 4000;
 
 
@@ -17,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors())
 
 app.use('/api/mailer', mailer)
 
